d33b97aa14032a6f7cc45487e1d3f27ba28b1c6c
Muhammad Aman <maman0022@outlook.com>
2020-10-22 10:35:02 -0500

    initial commit

---
+++ src/server.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+const app = require('./app')
+const { PORT, NODE_ENV } = require('./config')
+
+app.listen(PORT, () => {
+  console.log(`Server is in ${NODE_ENV} mode & listening on port ${PORT}`)
+})
